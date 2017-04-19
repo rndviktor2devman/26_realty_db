b790aad6b3db2df57696058fb4f982e352b85ee8
rndviktor2devman <7irb291wqkmu@mail.ru>
2017-04-20 01:10:50 +0300

    move search panel to js code

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,136 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-/**
- * Created by viktor on 4/17/17.
- */
+var ADList = React.createClass({displayName: "ADList",
+    //init state
+    getInitialState: function(){
+        return{
+            ads: [],
+        }
+    },
+
+    componentDidMount: function(){
+
+    },
+
+    render: function () {
+        return(React.createElement("div", null
+
+            )
+        )
+    }
+});
+
+
+var SearchPanel = React.createClass({displayName: "SearchPanel",
+    //init state
+    getInitialState: function(){
+        return{
+            priceFrom: null,
+            priceTo: null,
+            selectedCity: '',
+            selectedNew: false
+        }
+    },
+
+    componentDidMount: function(){
+
+    },
+
+    render: function () {
+        return(React.createElement("div", {className: "panel-body"}, 
+                  React.createElement("p", null, "город / районный центр"), 
+                  React.createElement("div", {className: "form-group"}, 
+                    React.createElement("select", {name: "oblast_district", className: "form-control"}, 
+                      React.createElement("option", {value: "Череповецкий район"}, "Череповец"), 
+                      React.createElement("option", {value: "Шекснинский район"}, "Шексна"), 
+                      React.createElement("option", {value: "Вологодский район"}, "Вологда"), 
+                      React.createElement("optgroup", {label: "Б"}, 
+                        React.createElement("option", {value: "Бабаевский район"}, "Бабаево"), 
+                        React.createElement("option", {value: "Бабушкинский район"}, "Село имени Бабушкина"), 
+                        React.createElement("option", {value: "Белозерский район"}, "Белозерск")
+                      ), 
+                      React.createElement("optgroup", {label: "В"}, 
+                        React.createElement("option", {value: "Великоустюгский район"}, "Великий Устюг"), 
+                        React.createElement("option", {value: "Верховажский район"}, "Верховажье"), 
+                        React.createElement("option", {value: "Вожегодский район"}, "Вожега"), 
+                        React.createElement("option", {value: "Вологодский район"}, "Вологда"), 
+                        React.createElement("option", {value: "Вытегорский район"}, "Вытегра")
+                      ), 
+                      React.createElement("optgroup", {label: "Г"}, 
+                        React.createElement("option", {value: "Грязовецкий район"}, "Грязовец")
+                      ), 
+                      React.createElement("optgroup", {label: "К"}, 
+                        React.createElement("option", {value: "Кадуйский район"}, "Кадуй"), 
+                        React.createElement("option", {value: "Кирилловский район"}, "Кириллов"), 
+                        React.createElement("option", {value: "Кичменгско-Городецкий район"}, "Кичменгский Городок")
+                      ), 
+                      React.createElement("optgroup", {label: "Л"}, 
+                        React.createElement("option", {value: "Вашкинский район"}, "Липин Бор")
+                      ), 
+                      React.createElement("optgroup", {label: "Н"}, 
+                        React.createElement("option", {value: "Никольский район"}, "Никольск"), 
+                        React.createElement("option", {value: "Нюксенский район"}, "Нюксеница")
+                      ), 
+                      React.createElement("optgroup", {label: "С"}, 
+                        React.createElement("option", {value: "Сокольский район"}, "Сокол"), 
+                        React.createElement("option", {value: "Сямженский район"}, "Сямжа")
+                      ), 
+                      React.createElement("optgroup", {label: "Т"}, 
+                        React.createElement("option", {value: "Тарногский район"}, "Тарногский Городок"), 
+                        React.createElement("option", {value: "Тотемский район"}, "Тотьма")
+                      ), 
+                      React.createElement("optgroup", {label: "У"}, 
+                        React.createElement("option", {value: "Усть-Кубинский район"}, "Устье"), 
+                        React.createElement("option", {value: "Устюженский район"}, "Устюжна")
+                      ), 
+                      React.createElement("optgroup", {label: "Х"}, 
+                        React.createElement("option", {value: "Харовский район"}, "Харовск")
+                      ), 
+                      React.createElement("optgroup", {label: "Ч"}, 
+                        React.createElement("option", {value: "Чагодощенский район"}, "Чагода"), 
+                        React.createElement("option", {value: "Череповецкий район"}, "Череповец")
+                      ), 
+                      React.createElement("optgroup", {label: "Ш"}, 
+                        React.createElement("option", {value: "Шекснинский район"}, "Шексна"), 
+                        React.createElement("option", {value: "Междуреченский район"}, "Шуйское")
+                      )
+                    )
+                  ), 
+                  React.createElement("div", {className: "form-group "}, 
+                    React.createElement("div", {className: "checkbox"}, 
+                      React.createElement("label", null, 
+                          React.createElement("input", {type: "checkbox", name: "new_building", value: "True"}), 
+                        "только новостройки"
+                      )
+                    )
+                  ), 
+                  React.createElement("p", null, React.createElement("strong", null, "Цена")), 
+                  React.createElement("div", {className: "form-group "}, 
+                    React.createElement("div", {className: "input-group"}, 
+                      React.createElement("span", {className: "input-group-addon"}, "от"), 
+                        React.createElement("input", {type: "text", value: "", name: "min_price", className: "form-control js-price-format", placeholder: "любая"}), 
+                      React.createElement("span", {className: "input-group-addon"}, "р.")
+                    )
+                  ), 
+                  React.createElement("div", {className: "form-group "}, 
+                    React.createElement("div", {className: "input-group"}, 
+                      React.createElement("span", {className: "input-group-addon"}, "до"), 
+                        React.createElement("input", {type: "text", value: "", name: "max_price", className: "form-control js-price-format", placeholder: "любая"}), 
+                      React.createElement("span", {className: "input-group-addon"}, "р.")
+                    )
+                  )
+                )
+        )
+    }
+});
+
+//ReactDOM.render(
+//    <ADList/>,
+//    document.getElementById('ads_panel')
+//);
+
+ReactDOM.render(
+    React.createElement(SearchPanel, null),
+    document.getElementById('search_panel')
+);
 
 },{}]},{},[1]);
--- static/jsx/main.js
+++ static/jsx/main.js
@@ -1,1 +1,133 @@
+var ADList = React.createClass({
+    //init state
+    getInitialState: function(){
+        return{
+            ads: [],
+        }
+    },
 
+    componentDidMount: function(){
+
+    },
+
+    render: function () {
+        return(<div>
+
+            </div>
+        )
+    }
+});
+
+
+var SearchPanel = React.createClass({
+    //init state
+    getInitialState: function(){
+        return{
+            priceFrom: null,
+            priceTo: null,
+            selectedCity: '',
+            selectedNew: false
+        }
+    },
+
+    componentDidMount: function(){
+
+    },
+
+    render: function () {
+        return(<div className="panel-body">
+                  <p>город / районный центр</p>
+                  <div className="form-group">
+                    <select name="oblast_district" className="form-control">
+                      <option value="Череповецкий район">Череповец</option>
+                      <option value="Шекснинский район">Шексна</option>
+                      <option value="Вологодский район">Вологда</option>
+                      <optgroup label="Б">
+                        <option value="Бабаевский район">Бабаево</option>
+                        <option value="Бабушкинский район">Село имени Бабушкина</option>
+                        <option value="Белозерский район">Белозерск</option>
+                      </optgroup>
+                      <optgroup label="В">
+                        <option value="Великоустюгский район">Великий Устюг</option>
+                        <option value="Верховажский район">Верховажье</option>
+                        <option value="Вожегодский район">Вожега</option>
+                        <option value="Вологодский район">Вологда</option>
+                        <option value="Вытегорский район">Вытегра</option>
+                      </optgroup>
+                      <optgroup label="Г">
+                        <option value="Грязовецкий район">Грязовец</option>
+                      </optgroup>
+                      <optgroup label="К">
+                        <option value="Кадуйский район">Кадуй</option>
+                        <option value="Кирилловский район">Кириллов</option>
+                        <option value="Кичменгско-Городецкий район">Кичменгский Городок</option>
+                      </optgroup>
+                      <optgroup label="Л">
+                        <option value="Вашкинский район">Липин Бор</option>
+                      </optgroup>
+                      <optgroup label="Н">
+                        <option value="Никольский район">Никольск</option>
+                        <option value="Нюксенский район">Нюксеница</option>
+                      </optgroup>
+                      <optgroup label="С">
+                        <option value="Сокольский район">Сокол</option>
+                        <option value="Сямженский район">Сямжа</option>
+                      </optgroup>
+                      <optgroup label="Т">
+                        <option value="Тарногский район">Тарногский Городок</option>
+                        <option value="Тотемский район">Тотьма</option>
+                      </optgroup>
+                      <optgroup label="У">
+                        <option value="Усть-Кубинский район">Устье</option>
+                        <option value="Устюженский район">Устюжна</option>
+                      </optgroup>
+                      <optgroup label="Х">
+                        <option value="Харовский район">Харовск</option>
+                      </optgroup>
+                      <optgroup label="Ч">
+                        <option value="Чагодощенский район">Чагода</option>
+                        <option value="Череповецкий район">Череповец</option>
+                      </optgroup>
+                      <optgroup label="Ш">
+                        <option value="Шекснинский район">Шексна</option>
+                        <option value="Междуреченский район">Шуйское</option>
+                      </optgroup>
+                    </select>
+                  </div>
+                  <div className="form-group ">
+                    <div className="checkbox">
+                      <label>
+                          <input type="checkbox" name="new_building" value="True"/>
+                        только новостройки
+                      </label>
+                    </div>
+                  </div>
+                  <p><strong>Цена</strong></p>
+                  <div className="form-group ">
+                    <div className="input-group">
+                      <span className="input-group-addon">от</span>
+                        <input type="text" value="" name="min_price" className="form-control js-price-format" placeholder="любая" />
+                      <span className="input-group-addon">р.</span>
+                    </div>
+                  </div>
+                  <div className="form-group ">
+                    <div className="input-group">
+                      <span className="input-group-addon">до</span>
+                        <input type="text" value="" name="max_price" className="form-control js-price-format" placeholder="любая" />
+                      <span className="input-group-addon">р.</span>
+                    </div>
+                  </div>
+                </div>
+        )
+    }
+});
+
+//ReactDOM.render(
+//    <ADList/>,
+//    document.getElementById('ads_panel')
+//);
+
+ReactDOM.render(
+    <SearchPanel/>,
+    document.getElementById('search_panel')
+);
